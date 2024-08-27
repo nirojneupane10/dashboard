69ca07786db3c5ca038cab26ddb0bc039a1df517
nirojneupane10 <niroj.neupane@varicon.com.au>
2024-08-27 15:56:29 +0545

    Added the error boundary

--- src/components/LazyLoad.tsx
+++ src/components/LazyLoad.tsx
@@ -1,10 +1,13 @@
 import { Suspense, ComponentType } from "react";
 import Loading from "./loader/Loading";
+import ErrorBoundary from "./ErrorBoundary";
 const LazyLoad = (Component: ComponentType) => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Component />
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <Component />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
